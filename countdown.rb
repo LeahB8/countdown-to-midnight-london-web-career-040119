e78d5e50bfbbaeb45ef184d1f0c5214212d07893
Leah Boyd <leahboyd08@gmail.com>
2019-03-17 18:26:01 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -5,7 +5,6 @@ def countdown(seconds_to_midnight)
   seconds_to_midnight = 10
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
-    break if seconds_to_midnight == 0
     seconds_to_midnight -= 1
   end
   return "HAPPY NEW YEAR!"
