28b3273b7b8969053dafabe88729d666237f275a
Leah Boyd <leahboyd08@gmail.com>
2019-03-17 18:24:53 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,15 +1,6 @@
 #write your code here
 
 
-def countdown(seconds_to_midnight)
-  seconds_to_midnight = 10
-  while seconds_to_midnight > 0
-    puts "#{seconds_to_midnight} SECOND(S)!"
-    break if seconds_to_midnight == 0
-    seconds_to_midnight -= 1
-  end
-  return "HAPPY NEW YEAR!"
-end 
 
 def countdown_with_sleep(seconds_to_midnight)
   seconds_to_midnight = 10
