0abf2ec20079ce2b1e18d83817d73028ec23ab97
Leah Boyd <leahboyd08@gmail.com>
2019-03-17 18:17:56 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,17 @@
 #write your code here
 
-def countdown
-end
+
+def countdown(number)
+  number = 10
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    break if number == 0
+    number -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end 
+
+def countdown_with_sleep(num_secs)
+  num_secs >= 5
+end  
+
