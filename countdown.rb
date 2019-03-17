7918d0a16e5754094e77770c7a20112338e6e372
Leah Boyd <leahboyd08@gmail.com>
2019-03-17 18:23:54 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -12,6 +12,7 @@ def countdown(seconds_to_midnight)
 end 
 
 def countdown_with_sleep(seconds_to_midnight)
+    seconds_to_midnight = 10
   while seconds_to_midnight > 0
     puts "#{seconds_to_midnight} SECOND(S)!"
     sleep(1)
